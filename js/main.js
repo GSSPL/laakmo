1159292227bcf928df2ed4b85564ef587039d4e3
uday1201 <udapra@gmail.com>
2017-02-25 19:12:47 +0530

    deploy8

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ jQuery(function($) {
 				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
 			}
 		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+			form_status.html('<p class="text-success">Thank you for contact us. As early as possible we will contact you</p>').delay(3000).fadeOut();
 		});
 	});
 
